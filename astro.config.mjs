a0d512d4f046f90ddf3868ca46773ec089dd0305
Brandon Corpas <corpasdev@gmail.com>
2025-06-20 19:02:03 -0500

    chore: update Astro configuration to include React and Astro Icon integrations, and set up path aliases for improved component imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,29 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
+import icon from "astro-icon";
+
 export default defineConfig({
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
   },
   site: 'https://brandoncorpas.com/',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(), 
+    react(), 
+    icon({
+      include: {
+        mdi: ["whatsapp"],
+      },
+    })
+  ],
+  alias: {
+    '@components/*': './src/components/*',
+    '@pages/*': './src/pages/*',
+    '@layouts/*': './src/layouts/*',
+    '@styles/*': './src/styles/*',
+  },
 });
